13b79a771011e3f353c5307ebf639a7d633725c0
Maksim Goleta <7947783+magole@users.noreply.github.com>
2024-04-17 20:18:58 -0700

    Add boilerplate code

---
+++ src/UidProvider.cs
@@ -0,0 +1,19 @@
+﻿// This is free and unencumbered software released into the public domain.
+// See the UNLICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neliva
+{
+
+    /// <summary>
+    /// Generates globally unique identifiers.
+    /// </summary>
+    public sealed class UidProvider
+    {
+    }
+}
---
+++ tests/UidProviderTests.cs
@@ -0,0 +1,16 @@
+// This is free and unencumbered software released into the public domain.
+// See the UNLICENSE file in the project root for more information.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Neliva.UidProvider.Tests
+{
+    [TestClass]
+    public class UidProviderTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

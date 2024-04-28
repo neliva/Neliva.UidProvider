6adf5c57a5c95b62cd3d7cba2c7b1204cf5096b1
Maksim Goleta <7947783+magole@users.noreply.github.com>
2024-04-27 17:23:29 -0700

    optimize UT

--- tests/UidProviderTests.cs
+++ tests/UidProviderTests.cs
@@ -34,55 +34,61 @@ namespace Neliva.Tests
 
         [TestMethod]
         [DynamicData(nameof(GetValidTestData), DynamicDataSourceType.Method)]
-        public void UidProviderFillPass(byte[] node, DateTime utcNow, ulong counter, byte[] rand)
+        public void UidProviderFillPass(ulong? node, ulong randNode, DateTime utcNow, ulong counter, byte[] randPart)
         {
-            int i = 0;
+            bool init = false;
 
-            var uid = new UidProvider(
-                node,
-                new UidUtcNowFunc(() => utcNow),
-                new UidRngFillAction((data) =>
-                {
-                    if (i++ == 0)
-                    {
-                        // Constructor initialize counter
-                        BinaryPrimitives.WriteUInt64BigEndian(data.Slice(8), counter);
-                    }
-                }));
+            Span<byte> expectedNode = stackalloc byte[8];
+            BinaryPrimitives.WriteUInt64BigEndian(expectedNode, node.HasValue ? node.Value : randNode);
+            expectedNode = expectedNode.Slice(2);
 
-            Span<byte> rd = stackalloc byte[16 + rand.Length];
-            Span<byte> tbytes = stackalloc byte[8];
-
-            uid.Fill(rd);
-
-            long timestampMs = (utcNow - DateTime.UnixEpoch).Ticks / TimeSpan.TicksPerMillisecond;
-
-            BinaryPrimitives.WriteUInt64BigEndian(tbytes, (ulong)timestampMs);
-
-            // timestamp
-            Assert.IsTrue(MemoryExtensions.SequenceEqual<byte>(tbytes.Slice(2, 6), rd.Slice(0, 6)));
-
-            // node
-            Assert.IsTrue(MemoryExtensions.SequenceEqual<byte>(node, rd.Slice(6, 6)));
-
-            // counter - internally already incremented
-            uint fillCount = BinaryPrimitives.ReadUInt32BigEndian(rd.Slice(12));
+            Span<byte> expectedTime = stackalloc byte[8];
+            BinaryPrimitives.WriteUInt64BigEndian(expectedTime, (ulong)((utcNow - DateTime.UnixEpoch).Ticks / TimeSpan.TicksPerMillisecond));
+            expectedTime = expectedTime.Slice(2);
 
             uint expectedCounter = (uint)counter + 1;
 
-            Assert.AreEqual(expectedCounter, fillCount);
+            var uid = new UidProvider(
+                node.HasValue ? expectedNode : Span<byte>.Empty,
+                new UidUtcNowFunc(() => utcNow),
+                new UidRngFillAction((data) =>
+                {
+                    if (!init) // Constructor initializer
+                    {
+                        BinaryPrimitives.WriteUInt64BigEndian(data.Slice(0), randNode);
+                        BinaryPrimitives.WriteUInt64BigEndian(data.Slice(8), counter);
+
+                        new Span<byte>(randPart).CopyTo(data.Slice(16));
+
+                        init = true;
+                    }
+                }));
+
+            Span<byte> output = stackalloc byte[16 + randPart.Length];
+
+            uid.Fill(output);
+
+            // timestamp
+            Assert.IsTrue(MemoryExtensions.SequenceEqual<byte>(expectedTime, output.Slice(0, 6)));
+
+            // node
+            Assert.IsTrue(MemoryExtensions.SequenceEqual<byte>(expectedNode, output.Slice(6, 6)));
+
+            // counter - internally already incremented
+            Assert.AreEqual(expectedCounter, BinaryPrimitives.ReadUInt32BigEndian(output.Slice(12, 4)));
 
             // random part
-            Assert.IsTrue(MemoryExtensions.SequenceEqual<byte>(rand, rd.Slice(16)));
+            Assert.IsTrue(MemoryExtensions.SequenceEqual<byte>(randPart, output.Slice(16)));
         }
 
         private static IEnumerable<object[]> GetValidTestData()
         {
-            // byte[] node, DateTime utcNow, ulong counter, byte[] rand
+            // ulong node, ulong randNode, DateTime utcNow, ulong counter, byte[] randPart
 
             yield return new object[]
             {
-                new byte[6],
+                null,
+                0ul,
                 DateTime.UnixEpoch,
                 (ulong)0,
                 Array.Empty<byte>(),
@@ -90,7 +96,8 @@ namespace Neliva.Tests
 
             yield return new object[]
             {
-                new byte[6],
+                0ul,
+                0ul,
                 DateTime.UnixEpoch,
                 (ulong)uint.MaxValue << 32,
                 new byte[0],
@@ -98,7 +105,8 @@ namespace Neliva.Tests
 
             yield return new object[]
             {
-                new byte[6],
+                0ul,
+                0ul,
                 DateTime.UnixEpoch,
                 (ulong)1,
                 new byte[0],
@@ -106,7 +114,8 @@ namespace Neliva.Tests
 
             yield return new object[]
             {
-                new byte[6],
+                0ul,
+                0ul,
                 DateTime.UnixEpoch,
                 (ulong)uint.MaxValue - 1,
                 new byte[0],
@@ -114,7 +123,8 @@ namespace Neliva.Tests
 
             yield return new object[]
             {
-                new byte[6],
+                0ul,
+                0ul,
                 DateTime.UnixEpoch,
                 (ulong)uint.MaxValue,
                 new byte[0],
@@ -122,7 +132,8 @@ namespace Neliva.Tests
 
             yield return new object[]
             {
-                new byte[6],
+                0ul,
+                0ul,
                 DateTime.UnixEpoch,
                 (ulong)ulong.MaxValue,
                 new byte[0],
@@ -130,7 +141,8 @@ namespace Neliva.Tests
 
             yield return new object[]
             {
-                new byte[6],
+                0ul,
+                0ul,
                 DateTime.UnixEpoch,
                 (ulong)0xff00ff00ff00ff00,
                 new byte[0],
@@ -138,7 +150,8 @@ namespace Neliva.Tests
 
             yield return new object[]
             {
-                new byte[6],
+                0ul,
+                0ul,
                 DateTime.UnixEpoch,
                 (ulong)0x1122334455667788,
                 new byte[0],

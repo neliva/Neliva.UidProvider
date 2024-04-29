9ce7f07a6b3e510c02a3c640c72710c8049191f5
Maksim Goleta <7947783+magole@users.noreply.github.com>
2024-04-28 22:57:57 -0700

    Magole/UidProvider (#2)

--- src/UidProvider.cs
+++ src/UidProvider.cs
@@ -2,18 +2,149 @@
 // See the UNLICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Threading;
 
 namespace Neliva
 {
-
     /// <summary>
-    /// Generates globally unique identifiers.
+    /// Provides functionality for generating unique across space and time identifiers.
     /// </summary>
+    /// <remarks>
+    /// <code>
+    /// +-------------+--------+-----------+----------+
+    /// |  Timestamp  |  Node  |  Counter  |  Random  |
+    /// +-------------+--------+-----------+----------+
+    /// |  6          |  6     |  4        |  0 - 16  |
+    /// +-------------+--------+-----------+----------+ 
+    /// </code>
+    /// </remarks>
     public sealed class UidProvider
     {
+        /// <summary>
+        /// Gets a default implementation of the <see cref="UidProvider"/> class that relies on
+        /// the <see cref="DateTime.UtcNow"/> and the <see cref="RandomNumberGenerator.Fill(Span{byte})"/>
+        /// to generate unique IDs.
+        /// </summary>
+        public static UidProvider Default { get; } = new UidProvider();
+
+        private readonly UidUtcNowFunc utcNowFunc;
+        private readonly UidRngFillAction rngFillAction;
+
+        private readonly byte node5;
+        private readonly byte node4;
+        private readonly byte node3;
+        private readonly byte node2;
+        private readonly byte node1;
+        private readonly byte node0;
+
+        private ulong counterRef;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UidProvider"/> class.
+        /// </summary>
+        /// <param name="node">
+        /// A 6 byte node value to embed in the unique identifier.
+        /// If not provided, a random value will be chosen.</param>
+        /// <param name="utcNow">
+        /// A callback that provides the current UTC date and time
+        /// to embed in the unique identifier.
+        /// </param>
+        /// <param name="rngFill">
+        /// A callback that fills a span with
+        /// cryptographically strong random bytes.
+        /// </param>
+        /// <exception cref="ArgumentException">
+        /// The <paramref name="node"/> span must be 6 bytes in length.
+        /// </exception>
+        public UidProvider(ReadOnlySpan<byte> node = default, UidUtcNowFunc utcNow = default, UidRngFillAction rngFill = default)
+        {
+            if (node.Length != 0 && node.Length != 6)
+            {
+                throw new ArgumentException("The span must be 6 bytes in length.", nameof(node));
+            }
+
+            this.utcNowFunc = utcNow ?? new UidUtcNowFunc(static () => DateTime.UtcNow);
+            this.rngFillAction = rngFill ?? new UidRngFillAction(static (data) => RandomNumberGenerator.Fill(data));
+
+            Span<byte> rd = stackalloc byte[16];
+
+            this.rngFillAction(rd);
+
+            this.counterRef =
+                  ((uint)rd[15]) |
+                  ((uint)rd[14] << 8) |
+                  ((uint)rd[13] << 16) |
+                  ((uint)rd[12] << 24);
+
+            var useNode = node.IsEmpty ? rd.Slice(2, 6) : node;
+
+            this.node5 = useNode[5];
+            this.node4 = useNode[4];
+            this.node3 = useNode[3];
+            this.node2 = useNode[2];
+            this.node1 = useNode[1];
+            this.node0 = useNode[0];
+        }
+
+        /// <summary>
+        /// Fills the span with the unique identifier bytes.
+        /// </summary>
+        /// <param name="data">
+        /// The span to fill with the unique identifier bytes.
+        /// </param>
+        /// <exception cref="ArgumentException">
+        /// The <paramref name="data"/> span must be between 16 and 32 bytes in length.
+        /// - or -
+        /// The <see cref="UidUtcNowFunc"/> callback value kind is not <see cref="DateTimeKind.Utc"/>,
+        /// or the value is before the <see cref="DateTime.UnixEpoch"/> value.
+        /// </exception>
+        public void Fill(Span<byte> data)
+        {
+            if (data.Length < 16 || data.Length > 32)
+            {
+                throw new ArgumentException("The span must be between 16 and 32 bytes in length.", nameof(data));
+            }
+
+            DateTime utcNow = this.utcNowFunc();
+
+            if (utcNow.Kind != DateTimeKind.Utc)
+            {
+                throw new ArgumentException("The date and time value kind must be UTC.");
+            }
+
+            if (utcNow < DateTime.UnixEpoch)
+            {
+                throw new ArgumentException("The date and time value must not be before the Unix epoch.");
+            }
+
+            long timestamp = (utcNow - DateTime.UnixEpoch).Ticks / TimeSpan.TicksPerMillisecond;
+
+            if (data.Length > 16)
+            {
+                this.rngFillAction(data.Slice(16));
+            }
+
+            ulong counter = Interlocked.Increment(ref this.counterRef);
+
+            data[15] = (byte)counter;
+            data[14] = (byte)(counter >> 8);
+            data[13] = (byte)(counter >> 16);
+            data[12] = (byte)(counter >> 24);
+
+            data[11] = node5;
+            data[10] = node4;
+            data[9] = node3;
+            data[8] = node2;
+            data[7] = node1;
+            data[6] = node0;
+
+            data[5] = (byte)timestamp;
+            data[4] = (byte)(timestamp >> 8);
+            data[3] = (byte)(timestamp >> 16);
+            data[2] = (byte)(timestamp >> 24);
+            data[1] = (byte)(timestamp >> 32);
+            data[0] = (byte)(timestamp >> 40);
+        }
     }
 }
--- tests/UidProviderTests.cs
+++ tests/UidProviderTests.cs
@@ -1,16 +1,283 @@
 // This is free and unencumbered software released into the public domain.
 // See the UNLICENSE file in the project root for more information.
 
+using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Neliva.UidProvider.Tests
+namespace Neliva.Tests
 {
     [TestClass]
     public class UidProviderTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void UidProviderDefaultPass()
         {
+            var d = UidProvider.Default;
+
+            Assert.IsTrue(object.ReferenceEquals(d, UidProvider.Default));
+
+            Span<byte> id1 = stackalloc byte[32];
+            Span<byte> id2 = stackalloc byte[32];
+
+            d.Fill(id1);
+            d.Fill(id2);
+
+            // random part
+            Assert.IsTrue(!MemoryExtensions.SequenceEqual<byte>(id1.Slice(16), id2.Slice(16)));
+
+            // count
+            Assert.IsTrue(!MemoryExtensions.SequenceEqual<byte>(id1.Slice(12, 4), id2.Slice(12, 4)));
+
+            // node
+            Assert.IsTrue(MemoryExtensions.SequenceEqual<byte>(id1.Slice(6, 6), id2.Slice(6, 6)));
+
+            var t1 = BinaryPrimitives.ReadUInt64BigEndian(id1) >> 8;
+            var t2 = BinaryPrimitives.ReadUInt64BigEndian(id2) >> 8;
+
+            Assert.IsTrue((t2 - t1) < 2000); // two calls should not be more than 2s apart.
+        }
+
+        [TestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        [DataRow(4)]
+        [DataRow(5)]
+        [DataRow(7)]
+        [DataRow(8)]
+        public void UidProviderCreateWithBadNodeFail(int nodeLength)
+        {
+            Assert.AreEqual("node", Assert.ThrowsException<ArgumentException>(() => new UidProvider(new byte[nodeLength])).ParamName);
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(15)]
+        [DataRow(33)]
+        [DataRow(48)]
+        public void UidProviderFillBadDataLengthFail(int dataLength)
+        {
+            var data = new byte[dataLength];
+
+            Assert.AreEqual("data", Assert.ThrowsException<ArgumentException>(() => UidProvider.Default.Fill(data)).ParamName);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(GetInvalidTimeKindTestData), DynamicDataSourceType.Method)]
+        public void UidProviderFillBadUtcNowKindFail(DateTime utcNow)
+        {
+            var data = new byte[16];
+
+            var uid = new UidProvider(utcNow: () => utcNow);
+
+            var ex = Assert.ThrowsException<ArgumentException>(() => uid.Fill(data));
+
+            Assert.AreEqual("The date and time value kind must be UTC.", ex.Message);
+        }
+
+        [TestMethod]
+        public void UidProviderFillBadUtcNowBeforeUnixEpochFail()
+        {
+            var data = new byte[16];
+
+            var utcNow = DateTime.UnixEpoch.AddMilliseconds(-1d);
+
+            var uid = new UidProvider(utcNow: () => utcNow);
+
+            var ex = Assert.ThrowsException<ArgumentException>(() => uid.Fill(data));
+
+            Assert.AreEqual("The date and time value must not be before the Unix epoch.", ex.Message);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(GetValidTestData), DynamicDataSourceType.Method)]
+        public void UidProviderFillPass(ulong? node, ulong randNode, DateTime utcNow, ulong counter, byte[] randPart)
+        {
+            bool isInitialized = false;
+
+            Span<byte> expectedNode = stackalloc byte[8];
+            BinaryPrimitives.WriteUInt64BigEndian(expectedNode, node.HasValue ? node.Value : randNode);
+            expectedNode = expectedNode.Slice(2);
+
+            Span<byte> expectedTime = stackalloc byte[8];
+            BinaryPrimitives.WriteUInt64BigEndian(expectedTime, (ulong)((utcNow - DateTime.UnixEpoch).Ticks / TimeSpan.TicksPerMillisecond));
+            expectedTime = expectedTime.Slice(2);
+
+            uint expectedCounter = (uint)counter + 1;
+
+            var uid = new UidProvider(
+                node.HasValue ? expectedNode : Span<byte>.Empty,
+                new UidUtcNowFunc(() => utcNow),
+                new UidRngFillAction((data) =>
+                {
+                    if (!isInitialized) // Constructor()
+                    {
+                        Assert.AreEqual(16, data.Length);
+
+                        BinaryPrimitives.WriteUInt64BigEndian(data.Slice(0), randNode);
+                        BinaryPrimitives.WriteUInt64BigEndian(data.Slice(8), counter);
+
+                        isInitialized = true;
+                    }
+                    else // Fill()
+                    {
+                        Assert.AreEqual(randPart.Length, data.Length);
+
+                        new Span<byte>(randPart).CopyTo(data);
+                    }
+                }));
+
+            Span<byte> output = stackalloc byte[16 + randPart.Length];
+
+            uid.Fill(output);
+
+            // timestamp
+            Assert.IsTrue(MemoryExtensions.SequenceEqual<byte>(expectedTime, output.Slice(0, 6)));
+
+            // node
+            Assert.IsTrue(MemoryExtensions.SequenceEqual<byte>(expectedNode, output.Slice(6, 6)));
+
+            // counter - internally already incremented
+            Assert.AreEqual(expectedCounter, BinaryPrimitives.ReadUInt32BigEndian(output.Slice(12, 4)));
+
+            // random part
+            Assert.IsTrue(MemoryExtensions.SequenceEqual<byte>(randPart, output.Slice(16)));
+        }
+
+        private static IEnumerable<object[]> GetValidTestData()
+        {
+            var maxDate = new DateTime(DateTime.MaxValue.Ticks, DateTimeKind.Utc);
+
+            // ulong node, ulong randNode, DateTime utcNow, ulong counter, byte[] randPart
+
+            yield return new object[]
+            {
+                null,
+                0ul,
+                DateTime.UnixEpoch,
+                0ul,
+                NewArray(0, 0),
+            };
+
+            yield return new object[]
+            {
+                null,
+                ulong.MaxValue,
+                DateTime.UnixEpoch.AddMilliseconds(1d),
+                1ul,
+                NewArray(0, 0),
+            };
+
+            yield return new object[]
+            {
+                null,
+                0xfful,
+                DateTime.UnixEpoch.AddYears(10),
+                (ulong)uint.MaxValue << 32,
+                NewArray(0, 0),
+            };
+
+            yield return new object[]
+            {
+                null,
+                0xfful << 56,
+                DateTime.UnixEpoch.AddYears(500),
+                (ulong)uint.MaxValue - 1,
+                NewArray(0, 0),
+            };
+
+            yield return new object[]
+            {
+                null,
+                0x1122334455667788ul,
+                maxDate.AddSeconds(-1d),
+                (ulong)uint.MaxValue,
+                NewArray(0, 0),
+            };
+
+            yield return new object[]
+            {
+                null,
+                0xd1d2d3d4d5d6d7d8ul,
+                maxDate.AddMilliseconds(-1d),
+                ulong.MaxValue,
+                NewArray(0, 0),
+            };
+
+            yield return new object[]
+            {
+                null,
+                0xe1e2e3e4e5e6e7e8ul,
+                maxDate,
+                0xf1f2f3f4f5f6f7f8ul,
+                NewArray(0, 0),
+            };
+
+            yield return new object[]
+            {
+                0ul,
+                0ul,
+                DateTime.UnixEpoch,
+                0ul,
+                NewArray(1, 0),
+            };
+
+            yield return new object[]
+            {
+                0x102030405060ul,
+                ulong.MaxValue,
+                DateTime.UnixEpoch,
+                0ul,
+                NewArray(16, 0),
+            };
+
+            yield return new object[]
+            {
+                0xa1a2a3a4a5a6ul,
+                0xfful,
+                DateTime.UnixEpoch,
+                0ul,
+                NewArray(1, 3),
+            };
+
+            yield return new object[]
+            {
+                0xb1b2b3b4b5b6b7b8ul,
+                0xfful << 56,
+                DateTime.UnixEpoch,
+                0ul,
+                NewArray(16, 5),
+            };
+
+            yield return new object[]
+            {
+                0xc1c2c3c4c5c6c7c8ul,
+                0x1122334455667788ul,
+                DateTime.UnixEpoch,
+                0ul,
+                NewArray(5, 7),
+            };
+        }
+
+        private static byte[] NewArray(int length, byte fillByte)
+        {
+            var a = length == 0 ? Array.Empty<byte>() : new byte[length];
+
+            Array.Fill(a, fillByte);
+
+            return a;
+        }
+
+        private static IEnumerable<object[]> GetInvalidTimeKindTestData()
+        {
+            yield return new object[] { new DateTime(DateTime.UnixEpoch.Ticks, DateTimeKind.Local) };
+            yield return new object[] { new DateTime(DateTime.UnixEpoch.Ticks, DateTimeKind.Unspecified) };
+
+            yield return new object[] { new DateTime(DateTime.UnixEpoch.Ticks - 1, DateTimeKind.Local) };
+            yield return new object[] { new DateTime(DateTime.UnixEpoch.Ticks - 1, DateTimeKind.Unspecified) };
         }
     }
 }

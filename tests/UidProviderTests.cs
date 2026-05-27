8100855cc066675abab4adfe1c96dec5a5997de4
Maksim Goleta <7947783+magole@users.noreply.github.com>
2026-05-26 21:12:54 -0700

    Migrate UTs to xUnit

--- tests/UidProviderTests.cs
+++ tests/UidProviderTests.cs
@@ -4,19 +4,18 @@
 using System;
 using System.Buffers.Binary;
 using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace Neliva.Tests
 {
-    [TestClass]
     public class UidProviderTests
     {
-        [TestMethod]
+        [Fact]
         public void UidProviderDefaultPass()
         {
             var d = UidProvider.System;
 
-            Assert.IsTrue(object.ReferenceEquals(d, UidProvider.System));
+            Assert.Same(d, UidProvider.System);
 
             Span<byte> id1 = stackalloc byte[32];
             Span<byte> id2 = stackalloc byte[32];
@@ -25,55 +24,56 @@ namespace Neliva.Tests
             d.Fill(id2);
 
             // random part
-            Assert.IsFalse(MemoryExtensions.SequenceEqual<byte>(id1.Slice(6), id2.Slice(6)));
+            Assert.False(MemoryExtensions.SequenceEqual<byte>(id1.Slice(6), id2.Slice(6)));
 
             // timestamp
             var t1 = BinaryPrimitives.ReadUInt64BigEndian(id1) >> 16;
             var t2 = BinaryPrimitives.ReadUInt64BigEndian(id2) >> 16;
-            Assert.IsTrue((t2 - t1) < 2000); // two calls should not be more than 2s apart.
+            Assert.True((t2 - t1) < 2000); // two calls should not be more than 2s apart.
         }
 
-        [TestMethod]
-        [DataRow(0)]
-        [DataRow(1)]
-        [DataRow(15)]
-        [DataRow(33)]
-        [DataRow(48)]
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(15)]
+        [InlineData(33)]
+        [InlineData(48)]
         public void UidProviderFillBadDataLengthFail(int dataLength)
         {
             var data = new byte[dataLength];
 
-            Assert.AreEqual("data", Assert.ThrowsException<ArgumentException>(() => UidProvider.System.Fill(data)).ParamName);
+            var ex = Assert.Throws<ArgumentException>(() => UidProvider.System.Fill(data));
+            Assert.Equal("data", ex.ParamName);
         }
 
-        [TestMethod]
-        [DynamicData(nameof(GetInvalidTimeKindTestData), DynamicDataSourceType.Method)]
+        [Theory]
+        [MemberData(nameof(GetInvalidTimeKindTestData))]
         public void UidProviderFillBadUtcNowKindFail(DateTime utcNow)
         {
             var data = new byte[16];
 
             var uid = new TestUidProvider(utcNow, new byte[10]);
 
-            var ex = Assert.ThrowsException<InvalidOperationException>(() => uid.Fill(data));
-            
-            Assert.AreEqual("The date and time value kind must be UTC.", ex.Message);
+            var ex = Assert.Throws<InvalidOperationException>(() => uid.Fill(data));
+
+            Assert.Equal("The date and time value kind must be UTC.", ex.Message);
         }
 
-        [TestMethod]
-        [DynamicData(nameof(GetInvalidUtcNowBeforeUnixTestData), DynamicDataSourceType.Method)]
+        [Theory]
+        [MemberData(nameof(GetInvalidUtcNowBeforeUnixTestData))]
         public void UidProviderFillBadUtcNowBeforeUnixEpochFail(DateTime utcNow)
         {
             var data = new byte[16];
 
             var uid = new TestUidProvider(utcNow, new byte[10]);
 
-            var ex = Assert.ThrowsException<InvalidOperationException>(() => uid.Fill(data));
-            
-            Assert.AreEqual("The date and time value must not be before the Unix epoch.", ex.Message);
+            var ex = Assert.Throws<InvalidOperationException>(() => uid.Fill(data));
+
+            Assert.Equal("The date and time value must not be before the Unix epoch.", ex.Message);
         }
 
-        [TestMethod]
-        [DynamicData(nameof(GetValidTestData), DynamicDataSourceType.Method)]
+        [Theory]
+        [MemberData(nameof(GetValidTestData))]
         public void UidProviderFillPass(DateTime utcNow, byte[] randPart)
         {
             Span<byte> expectedTime = stackalloc byte[8];
@@ -86,18 +86,18 @@ namespace Neliva.Tests
             uid.Fill(output);
 
             // timestamp
-            Assert.IsTrue(MemoryExtensions.SequenceEqual<byte>(expectedTime, output.Slice(0, 6)));
+            Assert.True(MemoryExtensions.SequenceEqual<byte>(expectedTime, output.Slice(0, 6)));
             // random part
-            Assert.IsTrue(MemoryExtensions.SequenceEqual<byte>(randPart, output.Slice(6)));
+            Assert.True(MemoryExtensions.SequenceEqual<byte>(randPart, output.Slice(6)));
 
             // Compare to .NET implementation of Unix time milliseconds
 
             long unixMilliseconds = (long)(BinaryPrimitives.ReadUInt64BigEndian(output) >> 16);
 
-            Assert.AreEqual(new DateTimeOffset(utcNow).ToUnixTimeMilliseconds(), unixMilliseconds);
+            Assert.Equal(new DateTimeOffset(utcNow).ToUnixTimeMilliseconds(), unixMilliseconds);
         }
 
-        [TestMethod]
+        [Fact]
         public void UidProviderUnixEpochZero()
         {
             var prov = new TestUidProvider(DateTime.UnixEpoch, NewArray(10, 128));
@@ -106,10 +106,10 @@ namespace Neliva.Tests
 
             prov.Fill(output);
 
-            Assert.IsTrue(MemoryExtensions.SequenceEqual<byte>(output.Slice(0, 6), new byte[6]));
+            Assert.True(MemoryExtensions.SequenceEqual<byte>(output.Slice(0, 6), new byte[6]));
         }
 
-        private static IEnumerable<object[]> GetValidTestData()
+        public static IEnumerable<object[]> GetValidTestData()
         {
             yield return new object[] { DateTime.UnixEpoch, NewArray(10, 10) };
             yield return new object[] { DateTime.UnixEpoch.AddTicks(1), NewArray(11, 11) };
@@ -122,21 +122,21 @@ namespace Neliva.Tests
             yield return new object[] { DateTime.UnixEpoch.AddMilliseconds(99d), NewArray(18, 18) };
         }
 
-        private static IEnumerable<object[]> GetInvalidTimeKindTestData()
+        public static IEnumerable<object[]> GetInvalidTimeKindTestData()
         {
             yield return new object[] { new DateTime(DateTime.UnixEpoch.Ticks, DateTimeKind.Local) };
             yield return new object[] { new DateTime(DateTime.UnixEpoch.Ticks, DateTimeKind.Unspecified) };
-            
+
             yield return new object[] { new DateTime(DateTime.UnixEpoch.Ticks - 1, DateTimeKind.Local) };
             yield return new object[] { new DateTime(DateTime.UnixEpoch.Ticks - 1, DateTimeKind.Unspecified) };
         }
 
-        private static IEnumerable<object[]> GetInvalidUtcNowBeforeUnixTestData()
+        public static IEnumerable<object[]> GetInvalidUtcNowBeforeUnixTestData()
         {
             yield return new object[] { DateTime.UnixEpoch.AddTicks(-1) };
             yield return new object[] { DateTime.UnixEpoch.AddMilliseconds(-1d) };
-
         }
+
         private static byte[] NewArray(int length, byte fillByte)
         {
             var a = length == 0 ? Array.Empty<byte>() : new byte[length];
